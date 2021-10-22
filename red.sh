d3344ec523976ddc1c683e8e59091b051aed1cfe
Slownite <snfdiop@outlook.com>
2021-10-22 14:36:50 +0200

    detection of path

--- red.sh
+++ red.sh
@@ -1,5 +1,6 @@
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+RED_CURRENT_PROJECT=$(pwd)
 open -a XQuartz
 socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
-docker run -it  -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v /:$1 red
+docker run -it  -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $RED_CURRENT_PROJECT:$1 red
 
